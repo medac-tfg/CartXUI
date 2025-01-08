76d06c75eae0a24bcc799875fda5cb2132856b81
Cristian Olivares (visibait) <grafoplays@gmail.com>
2025-01-08 10:12:05 +0100

    fix(forge): Fix serialport build

--- forge.config.ts
+++ forge.config.ts
@@ -7,6 +7,9 @@ import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import { spawn } from "child_process";
+import path from "path";
+import fs from "fs";
 
 import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
@@ -60,6 +63,73 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  // Fix serialport native module not working
+  hooks: {
+    packageAfterPrune: async (_, buildPath, __, platform) => {
+      const commands = [
+        "install",
+        "--no-package-lock",
+        "--no-save",
+        "serialport",
+      ];
+
+      return new Promise((resolve, reject) => {
+        const oldPckgJson = path.join(buildPath, "package.json");
+        const newPckgJson = path.join(buildPath, "_package.json");
+
+        fs.renameSync(oldPckgJson, newPckgJson);
+
+        const npmInstall = spawn("npm", commands, {
+          cwd: buildPath,
+          stdio: "inherit",
+          shell: true,
+        });
+
+        npmInstall.on("close", (code) => {
+          if (code === 0) {
+            fs.renameSync(newPckgJson, oldPckgJson);
+
+            /**
+             * On windows code signing fails for ARM binaries etc.,
+             * we remove them here
+             */
+            if (platform === "win32") {
+              const problematicPaths = [
+                "android-arm",
+                "android-arm64",
+                "darwin-x64+arm64",
+                "linux-arm",
+                "linux-arm64",
+                "linux-x64",
+              ];
+
+              problematicPaths.forEach((binaryFolder) => {
+                fs.rmSync(
+                  path.join(
+                    buildPath,
+                    "node_modules",
+                    "@serialport",
+                    "bindings-cpp",
+                    "prebuilds",
+                    binaryFolder
+                  ),
+                  { recursive: true, force: true }
+                );
+              });
+            }
+
+            resolve();
+          } else {
+            reject(new Error("process finished with error code " + code));
+          }
+        });
+
+        npmInstall.on("error", (error) => {
+          reject(error);
+        });
+      });
+    },
+  },
 };
 
 export default config;
